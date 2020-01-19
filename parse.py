73c6a13c63cd2051d28163e4784ca2ede6e958c6
Frank <alchemistWu0521@gmail.com>
2020-01-19 12:45:39 -0500

    new

--- parse.py
+++ parse.py
@@ -4,29 +4,49 @@ import os
 import json
 from math import sin, cos, sqrt, atan2, radians
 from geopy import distance
+import pdb
+import datetime
 
 def get_dist(loc1,loc2):
     # approximate radius of earth in km
     R = 6373.0
-    '''
-    lat1 = radians(loc1[0])
-    lon1 = radians(loc1[1])
-    lat2 = radians(loc2[0])
-    lon2 = radians(loc2[1])
-    '''
+
     dist = distance.distance(loc1,loc2).km
 
     dist = dist * 0.53996 #convert to knot
     return dist
 
+
+
 def get_time_diff(t1,t2):
     '''
     ret: time in secs
     '''
-    #print(t1)
-    #print(t2)
-    return (t2[0] - t1[0]) + (t2[1] - t1[1])/60 + (t2[2] - t1[2])/3600 
-
+    h_t1 = t1[0]
+    h_t2 = t2[0]
+    if h_t1 == 24:
+        h_t1 = 0
+    if h_t2 == 24:
+        h_t2 = 0
+    m_t1 = t1[1]
+    m_t2 = t2[1]
+    if m_t1 == 60:
+        m_t1 = 0
+    if m_t2 == 60:
+        m_t2 = 0
+    s_t1 = t1[1]
+    s_t2 = t2[1]
+    if s_t1 == 60:
+        s_t1 = 0
+    if s_t2 == 60:
+        s_t2 = 0
+    t1 = datetime.datetime(year=2012, month=3, day=25, hour=h_t1, minute=m_t1, second=s_t1)
+    t2 = datetime.datetime(year=2012, month=3, day=25, hour=h_t2, minute=m_t2, second=s_t2)
+    if t2 < t1:
+        t2 = datetime.datetime(year=2012, month=3, day=26, hour=h_t2, minute=m_t2, second=s_t2)
+    diff = (t2 - t1).seconds
+    #print(diff)
+    return float(diff) / 3600.
 
 def parse_raw(path):
     with open(path,'r') as f:
@@ -70,11 +90,11 @@ def add_speed(in_file):
                 t_cur = (msg['hour'],msg['minute'],msg['second'])
                 time_h = get_time_diff(t_prev,t_cur)
                 if time_h != 0:
-                    msg['speed'] = get_dist(prev_dic[msg['mmsi']]['loc'],(msg['y'],msg['x']))/time_h
+                    d = get_dist(prev_dic[msg['mmsi']]['loc'],(msg['y'],msg['x']))
+                    msg['speed'] = d/time_h
+                    #if msg['speed']>10:
+                    #    print(msg['speed'])
                     prev_dic[msg['mmsi']] = {'loc':(msg['y'],msg['x']),'time':t_cur}
-                    if msg['speed'] > 500:
-                        print(t_prev)
-                        print(t_cur)
                 else:
                     msg['speed'] = -1
             yield msg
